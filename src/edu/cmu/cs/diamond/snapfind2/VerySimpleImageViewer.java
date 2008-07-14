afe749a7f803beb79cf10209ee7fbe0142a915fe
Adam Goode <agoode@andrew.cmu.edu>
2008-07-14 15:17:37 -0400

    Allow selection of image channels

--- src/edu/cmu/cs/diamond/snapfind2/VerySimpleImageViewer.java
+++ src/edu/cmu/cs/diamond/snapfind2/VerySimpleImageViewer.java
@@ -2,29 +2,75 @@ package edu.cmu.cs.diamond.snapfind2;
 
 import java.awt.BorderLayout;
 import java.awt.Graphics2D;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class VerySimpleImageViewer extends JFrame {
-    public VerySimpleImageViewer(AnnotatedResult result, BufferedImage imgs[]) {
-        BufferedImage img = imgs[0];
-        Graphics2D g = img.createGraphics();
-        result.decorate(g, 1.0);
-        g.dispose();
+    public class ChannelSelector extends JPanel {
+        public ChannelSelector() {
+            String labels[] = new String[imgs.length];
+            for (int i = 0; i < labels.length; i++) {
+                labels[i] = "Image " + (i + 1);
+            }
 
-        JLabel p = new JLabel(new ImageIcon(img));
-        JScrollPane jsp = new JScrollPane(p);
+            setLayout(new BorderLayout());
+            final JList list = new JList(labels);
+
+            list.setSelectedIndex(0);
+            list.getSelectionModel().setSelectionMode(
+                    ListSelectionModel.SINGLE_SELECTION);
+            list.addListSelectionListener(new ListSelectionListener() {
+                public void valueChanged(ListSelectionEvent e) {
+                    if (e.getFirstIndex() != -1) {
+                        setImage(result, imgs[list.getSelectedIndex()]);
+                    }
+                }
+            });
+            add(list);
+        }
+    }
+
+    final private BufferedImage[] imgs;
+
+    final private JLabel image;
+
+    final private AnnotatedResult result;
+
+    public VerySimpleImageViewer(AnnotatedResult result, BufferedImage imgs[]) {
+        this.result = result;
+        this.imgs = imgs;
+        image = new JLabel();
+        JScrollPane jsp = new JScrollPane(image);
         jsp.getVerticalScrollBar().setUnitIncrement(40);
         jsp.getHorizontalScrollBar().setUnitIncrement(40);
         add(jsp);
-        add(new JLabel(result.getAnnotation()), BorderLayout.SOUTH);
+
+        setImage(result, imgs[0]);
+
+        JPanel bottomPanel = new JPanel(new GridLayout(1, 2));
+        bottomPanel.add(new JLabel(result.getAnnotation()));
+        bottomPanel.add(new ChannelSelector());
+
+        add(bottomPanel, BorderLayout.SOUTH);
 
         pack();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationByPlatform(true);
+    }
+
+    private void setImage(AnnotatedResult result, BufferedImage img) {
+        BufferedImage newImage = new BufferedImage(img.getWidth(), img
+                .getHeight(), img.getType());
+        Graphics2D g = newImage.createGraphics();
+        g.drawImage(img, 0, 0, null);
+        result.decorate(g, 1.0);
+        g.dispose();
+        image.setIcon(new ImageIcon(newImage));
+
+        validate();
     }
 }
