9ec38355509fa97250773a0c803c8b2c5ec56ef5
Adam Goode <agoode@andrew.cmu.edu>
2008-07-22 15:58:27 -0500

    Update licenses for StrangeFind native code

--- native/util.c
+++ native/util.c
@@ -1,15 +1,41 @@
 /*
- *  FatFind
- *  A Diamond application for adipocyte image exploration
- *  Version 1
+ *  StrangeFind, an anomaly detection system for the OpenDiamond Platform
  *
- *  Copyright (c) 2006 Carnegie Mellon University
- *  All Rights Reserved.
+ *  Copyright (c) 2006-2008 Carnegie Mellon University
+ *  All rights reserved.
  *
- *  This software is distributed under the terms of the Eclipse Public
- *  License, Version 1.0 which can be found in the file named LICENSE.
- *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
- *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ *  StrangeFind is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2.
+ *
+ *  StrangeFind is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with StrangeFind. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Linking StrangeFind statically or dynamically with other modules is
+ *  making a combined work based on StrangeFind. Thus, the terms and
+ *  conditions of the GNU General Public License cover the whole
+ *  combination.
+ *
+ *  In addition, as a special exception, the copyright holders of
+ *  StrangeFind give you permission to combine StrangeFind with free software
+ *  programs or libraries that are released under the GNU LGPL or the
+ *  Eclipse Public License 1.0. You may copy and distribute such a system
+ *  following the terms of the GNU GPL for StrangeFind and the licenses of
+ *  the other code concerned, provided that you include the source code of
+ *  that other code when and as the GNU GPL requires distribution of source
+ *  code.
+ *
+ *  Note that people who make modified versions of StrangeFind are not
+ *  obligated to grant this special exception for their modified versions;
+ *  it is their choice whether to do so. The GNU General Public License
+ *  gives permission to release a modified version without this exception;
+ *  this exception also makes it possible to release a modified version
+ *  which carries forward this exception.
  */
 
 #ifdef HAVE_CONFIG_H
